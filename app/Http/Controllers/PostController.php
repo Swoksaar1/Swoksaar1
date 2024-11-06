0cc9c1dcbc9f0c507d102119fcb27682f2a25695
Swoksaar1 <lagamonrenz@gmail.com>
2024-11-06 21:02:15 +0800

    Create PostController.php

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Post;
+
+class PostController extends Controller
+{
+    public function index()
+    {
+        $posts = Post::all();
+        return view('posts.index', compact('posts'));
+    }
+
+    public function show($id)
+    {
+        $post = Post::findOrFail($id);
+        return view('posts.show', compact('post'));
+    }
+
+    public function create()
+    {
+        return view('posts.create');
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'title' => 'required|max:255',
+            'body' => 'required',
+        ]);
+
+        Post::create($request->all());
+
+        return redirect()->route('home');
+    }
+}
